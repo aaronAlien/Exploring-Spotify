8fbf532fbba7d01bc9d3974f2c238681e037199d
Aaron <thatshakespeare2@gmail.com>
2025-02-21 14:59:01 +0000

    initial

---
+++ Convert_CSV/convert.py
@@ -0,0 +1,107 @@
+import re
+import csv
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+from PIL import Image, ImageTk
+
+def convert_file(input_file):
+    # Read raw data
+    with open(input_file, 'r', encoding='utf-8') as file:
+        data = file.readlines()
+
+    # Regex pattern
+    pattern = re.compile(r'time="([\d\-:\s]+)"\s+type=([A-Z_]+)\s+package=([a-zA-Z0-9\.]+)')  # for com.apps
+
+    csv_data = [['Timestamp', 'Activity Type', 'Application']]
+
+    # Loop through lines - append time and duration
+    for line in data:
+        match = pattern.search(line)
+        if match:
+            timestamp, activity_type, duration = match.groups()
+            csv_data.append([timestamp, activity_type, duration])
+
+    # user save location
+    output_file = filedialog.asksaveasfilename(
+        defaultextension=".csv",
+        filetypes=[("CSV files", "*.csv")],
+        title="Save As"
+    )
+
+    if not output_file:  # User canceled the save dialog
+        return None
+
+    # to CSV
+    try:
+        with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(csv_data)
+        return output_file
+    except Exception as e:
+        messagebox.showerror("Error", f"Failed to save file: {e}")
+        return None
+
+def select_file():
+    file_path = filedialog.askopenfilename(title="Select a text file", filetypes=[("Text files", "*.txt")])
+    if file_path:
+        try:
+            output_file = convert_file(file_path)
+            if output_file:  # if location chosen
+                messagebox.showinfo("Success", f"File converted successfully!\nSaved as: {output_file}")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
+
+root = tk.Tk()
+root.title("TXT to CSV Converter")
+root.geometry("480x480")
+root.configure(bg="#000000")
+root.resizable(False, False)  # Allows resizing for testing, set to False for no resize
+
+# Background
+bg_original = Image.open("assets/background.png")
+bg_photo = ImageTk.PhotoImage(bg_original)
+
+bg_label = tk.Label(root, image=bg_photo)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+# button hover functions
+def on_enter(event):
+    convert_button.config(fg="#95CF3F")
+
+def on_leave(event):
+    convert_button.config(fg="#ffffff")
+
+# style
+btn_style = {
+    "font": ("Terminal", 22, "bold"),
+    "bg": "#000000",
+    "fg": "#ffffff",
+    "padx": 28,
+    "pady": 20,
+    "bd": 0.5,
+    "relief": "raised",
+    "highlightthickness": 0,
+    "overrelief": "groove",
+}
+head_style = {
+    "font": ("Terminal", 20, "bold"),
+    "bg": "#000000",
+    "fg": "#ffffff",
+    "padx": 34,
+    "pady": 10,
+}
+
+# Widgets
+heading = tk.Label(root, text='-> Select file \n-> Choose save location', **head_style)
+heading.place(relx=0.5, rely=0.1, anchor="center")
+
+convert_button = tk.Button(root, text="SELECT FILE", command=select_file, **btn_style)
+convert_button.place(relx=0.5, rely=0.485, anchor="center")
+
+# hover button
+convert_button.bind("<Enter>", on_enter)  
+convert_button.bind("<Leave>", on_leave)
+
+
+root.mainloop()
---
+++ playlist.py
@@ -0,0 +1,42 @@
+import spotipy
+import os
+import random
+import pandas as pd
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+# creates a random playlist from my_playlist_tracks.csv
+
+def create_playlist(csv_file, playlist_name, tracks_num=10):
+
+    df = pd.read_csv('data/my_playlist_tracks.csv')
+
+    # auth and token
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id = os.getenv('SPOTIFY_CLIENT_ID'),
+        client_secret = os.getenv('SPOTIFY_CLIENT_SECRET'),
+        redirect_uri='http://localhost:8000/callback',
+        scope='user-top-read, playlist-read-private, playlist-modify-public',
+        #cache_path=cache_path
+    ))
+
+    user_id = sp.current_user()['id']
+    
+    # get track ids
+    track_ids = df['track id'].tolist()
+
+    # random selection
+    selected_tracks = random.sample(track_ids, min(tracks_num, len(track_ids)))
+
+    playlist_name = 'Random Playlist from AHS'
+    playlist_description = f"Random playlist from {csv_file} with {tracks_num} tracks."
+    playlist = sp.user_playlist_create(user_id, playlist_name, description=playlist_description)
+
+    # add using uri
+    track_uris = [f'spotify:track:{track_id}' for track_id in selected_tracks]
+    sp.playlist_add_items(playlist['id'], track_uris)
+
+    # done
+    print(f'{playlist_name} created successfully!')
+
+create_playlist('data/my_playlist_tracks.csv', playlist_name='Random Playlist from .csv', tracks_num=20)
